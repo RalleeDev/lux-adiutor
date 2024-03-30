3d612a1389e4c03917dca7f45d944670d32a2f9b
RalleeDev <109039505+RalleeDev@users.noreply.github.com>
2024-03-30 01:46:16 +0100

    Created Todo.md

--- src/changeLightState.java
+++ src/changeLightState.java
@@ -1,17 +1,4 @@
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-
 public class changeLightState {
     public static void main(String[] args) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=56.1567&longitude=10.2108&hourly=temperature_2m,visibility&timezone=Europe%2FBerlin&forecast_days=1"))
-            .build();
-        System.out.println("status code: " + response.statusCode());
-        System.out.println("headers: " + response.headers());
-        System.out.println("response body: " + response.body());
     }
 }
---
+++ todo.md
@@ -0,0 +1,10 @@
+# Todo
+
+Here we have a list of all the features that needs implementing and all implemented features
+
+### Version 1
+| Feature | Status |
+| ------- | ------ |
+| Get all lights and rooms for more functionality | W,I.P |
+| Change light state (toggle on/off, brightness etc) | Not ready | 
+| CLI for all the features and settings | Not Ready
